3f0d836788d513a9afa125faaf1a2ad8d0d4c0c9
farzaank <fkaiyom@gmail.com>
2023-11-27 15:10:13 -0800

    fix tmf

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,11 @@ import getBenchmarkSuite from "@/utils/getBenchmarkSuite";
 //import getBenchmarkSuite from "@/utils/getBenchmarkSuite";
 
 export default function Footer() {
-	const version = getBenchmarkRelease();
-	const suite = getBenchmarkSuite();
-	return (
-		<div className="bottom-0 right-0 p-4 bg-white-800 text-black text-right">
-			{suite ? <p>Suite {suite}</p> : <p>Release {version}</p>}
-		</div>
-	);
+  const version = getBenchmarkRelease();
+  const suite = getBenchmarkSuite();
+  return (
+    <div className="bottom-0 right-0 p-4 bg-white-800 text-black text-right">
+      {suite ? <p>Suite {suite}</p> : <p>Release {version}</p>}
+    </div>
+  );
 }

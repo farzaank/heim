b2de21232544723eb029f5b1bfff3d9133ba2395
farzaank <fkaiyom@gmail.com>
2023-11-27 15:10:04 -0800

    fix footer for legacy suite

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
 import getBenchmarkRelease from "@/utils/getBenchmarkRelease";
+import getBenchmarkSuite from "@/utils/getBenchmarkSuite";
 //import getBenchmarkSuite from "@/utils/getBenchmarkSuite";
 
 export default function Footer() {
-  const version = getBenchmarkRelease();
-  return (
-    <div className="bottom-0 right-0 p-4 bg-white-800 text-black text-right">
-      <p>Release {version}</p>
-    </div>
-  );
+	const version = getBenchmarkRelease();
+	const suite = getBenchmarkSuite();
+	return (
+		<div className="bottom-0 right-0 p-4 bg-white-800 text-black text-right">
+			{suite ? <p>Suite {suite}</p> : <p>Release {version}</p>}
+		</div>
+	);
 }

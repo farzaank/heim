92b3ceee7ff8c49a9a60548cd2e269e89877505a
farzaank <fkaiyom@gmail.com>
2024-02-15 18:32:04 -0800

    add schema fallback

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
-window.BENCHMARK_OUTPUT_BASE_URL =
-	"https://nlp.stanford.edu/helm/v1.1.0/benchmark_output/"; // FOR NOW NEED TO UPDATE THIS WITH RELEASE ANYTIME WE CHANGE RELEASE FOR HEIM
-window.SUITE = "v1.1.0"; // IF YOU UPDATE THIS, ALSO UPDATE BENCHMARK_OUTPUT_BASE_URL
+window.SUITE = "v1.1.0";
 window.RELEASE = null;
+window.BENCHMARK_OUTPUT_BASE_URL =
+	window.BENCHMARK_OUTPUT_BASE_URL = `https://nlp.stanford.edu/helm/${window.SUITE}/benchmark_output/`;
 window.MANIFEST_JSON_URL = null;
 window.MANIFEST_ID = 0;
